0dd095c5e64e3834e9961dd0e0c5435531d2fab1
Adi Harif <adi.harif@campus.technion.ac.il>
2024-06-24 11:58:02 +0300

    Adds list_join_by_string functor

--- src/functors/list_functors.cpp
+++ src/functors/list_functors.cpp
@@ -2,6 +2,7 @@
 #include <souffle/SouffleInterface.h>
 
 using namespace souffle;
+using std::string;
 
 extern "C" {
 
@@ -28,6 +29,24 @@ RamDomain list_contains(SymbolTable* symbolTable, RecordTable* recordTable, RamD
         return 1;
     }
     return list_contains(symbolTable, recordTable, tl, value);
+}
+
+RamDomain list_join_by_string(SymbolTable* symbolTable, RecordTable* recordTable, RamDomain listId, RamDomain separator) {
+    if (listId == 0) { // list1 is nil
+        return listId;
+    }
+    const RamDomain* list = recordTable->unpack(listId, 2);
+    RamDomain hd = list[0];
+    RamDomain tl = list[1];
+    string out = symbolTable->decode(hd);
+    while (tl != 0) {
+        list = recordTable->unpack(tl, 2);
+        hd = list[0];
+        tl = list[1];
+        out += symbolTable->decode(separator);
+        out += symbolTable->decode(hd);
+    }
+    return symbolTable->encode(out);
 }
 
 }
